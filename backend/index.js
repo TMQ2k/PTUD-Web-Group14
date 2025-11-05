bfe6fd39a024ff5a42e031bb67a5cc61e70e6ae5
Phon <ndphon23@clc.fitus.edu.vn>
2025-11-05 22:16:59 +0700

    git config database connection and checking connection

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import pool from "./src/db.js";
+import pool from "./src/database/db.js";
 
 dotenv.config();
 
