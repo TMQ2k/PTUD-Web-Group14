6345c84051bc1cac3709aad855bfe559acb99749
Phon <ndphon23@clc.fitus.edu.vn>
2025-11-13 10:53:13 +0700

    database add index and data file

--- backend/src/database/schema.sql
+++ backend/src/database/schema.sql
@@ -37,7 +37,27 @@ CREATE TABLE users_rating (
     ) STORED
 );
 
+CREATE TABLE user_upgrade_requests (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
 
+    cur_role VARCHAR(50) NOT NULL,
+    requested_role VARCHAR(50) NOT NULL,
+
+    reason TEXT,
+    attachments JSONB,  -- có thể chứa danh sách URL hoặc metadata file
+    status VARCHAR(20) NOT NULL DEFAULT 'pending' 
+        CHECK (status IN ('pending', 'approved', 'rejected', 'cancelled')),
+
+    reviewed_by BIGINT REFERENCES users(user_id) ON DELETE SET NULL,
+    review_comment TEXT,
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    reviewed_at TIMESTAMPTZ,
+
+    CONSTRAINT chk_different_role CHECK (cur_role <> requested_role)
+);
 
 CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
@@ -77,3 +97,35 @@ CREATE TABLE user_otp (
     otp_code VARCHAR(6) NOT NULL,
     expires_at TIMESTAMP NOT NULL
 );
+
+CREATE TABLE bids (
+    id BIGSERIAL PRIMARY KEY,
+    product_id BIGINT NOT NULL REFERENCES products(product_id) ON DELETE CASCADE,
+    user_id BIGINT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+
+    amount NUMERIC(12,2) NOT NULL CHECK (amount > 0),
+    bid_time TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    status VARCHAR(20) NOT NULL DEFAULT 'active'
+        CHECK (status IN ('active', 'winning', 'outbid', 'rejected')),
+
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE auto_bids (
+    id BIGSERIAL PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    product_id BIGINT NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+
+    max_bid_amount NUMERIC(12, 2) NOT NULL CHECK (max_bid_amount > 0),
+    current_bid_amount NUMERIC(12, 2) DEFAULT 0 CHECK (current_bid_amount >= 0),
+    
+    is_active BOOLEAN DEFAULT TRUE,
+    is_winner BOOLEAN DEFAULT FALSE,
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+
+    UNIQUE (user_id, product_id)
+);
+
